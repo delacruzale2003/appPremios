4b74a646686cec5cd672533988489f74b97715f5
Alejandro <delacruzcalderonalejandro23@gmail.com>
2025-09-23 19:27:45 -0500

    registro controller updated

--- controllers/registroController.js
+++ controllers/registroController.js
@@ -2,20 +2,25 @@ const Registro = require('../models/Registro'); // Importar el modelo Registro
 
 // Función para obtener todos los registros con los detalles relacionados
 exports.getRegistros = async (req, res) => {
-    try {
-        // Obtener los registros de los clientes y poblar las relaciones
-        const registros = await Registro.find()
-            .populate('cliente_id', 'nombre dni')  // Poblar cliente_id y obtener solo nombre y dni
-            .populate('tienda_id', 'nombre')      // Poblar tienda_id y obtener solo el nombre de la tienda
-            .populate('premio_id', 'nombre')      // Poblar premio_id y obtener solo el nombre del premio
-            .exec();
+  try {
+    const limit = parseInt(req.query.limit) || 10; // por defecto 10 registros
+    const skip = parseInt(req.query.skip) || 0;
 
-        // Responder con los registros encontrados
-        res.status(200).json({ registros });
-    } catch (error) {
-        // Manejar el error y devolver una respuesta con estado 500
-        res.status(500).json({ message: 'Error al obtener registros', error });
-    }
+    const registros = await Registro.find()
+      .sort({ fecha_registro: -1 }) // opcional: ordena por fecha
+      .limit(limit)
+      .skip(skip)
+      .populate('cliente_id', 'nombre dni telefono') // incluye teléfono
+      .populate('tienda_id', 'nombre')
+      .populate('premio_id', 'nombre')
+      .exec();
+
+    const total = await Registro.countDocuments();
+
+    res.status(200).json({ registros, total });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener registros', error });
+  }
 };
 
 exports.getRegistroById = async (req, res) => {
