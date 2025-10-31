fee751398b12fcae79dcc861d3fa42161b012728
Alejandro <delacruzcalderonalejandro23@gmail.com>
2025-10-31 18:57:41 -0500

    registrocompleto  added v.1.3

--- routes/registroRoutes.js
+++ routes/registroRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const registroController = require('../controllers/registroController');
 
 router.get('/cliente/:idCliente', registroController.getRegistroPorCliente); // ← primero
+router.get('/completos', registroController.getRegistrosCompletos); // ✅ nuevo endpoint
 router.get('/', registroController.getRegistros);
 router.get('/:id', registroController.getRegistroById); // ← último
 
